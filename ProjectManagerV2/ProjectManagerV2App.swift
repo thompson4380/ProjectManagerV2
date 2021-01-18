c081f60f266c1496a1892860757ee5649dbca908
Thomas Ostermann <t.ostermann@txonline.de>
2021-01-18 20:13:53 +0100

    Initial Commit

---
+++ ProjectManagerV2/ProjectManagerV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerV2App.swift
+//  ProjectManagerV2
+//
+//  Created by Thomas Ostermann on 18.01.21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
